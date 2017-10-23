bcc0bc3e4d692a75c26eacdc4a9f15752487818e
therealplato <therealplato@gmail.com>
2017-10-23 21:38:35 +1300

    soberoctober23 static

--- hugosite/static/js/soberoctober23.js
+++ hugosite/static/js/soberoctober23.js
@@ -1,3 +1,40 @@
 // All D3 code goes here. For Example:
 console.log(d3.version); // 4.7.4
-d3.select("#laptimes-container");
+var url = "/misc/suzuka-laps.json";
+d3.json(url, function (data) {
+  console.log(data)
+  var bars = d3.select("#laptimes-container")
+    .selectAll("span")
+    .data(data);
+
+  var tmax = d3.max(data, function(d, i) {
+    return d.timeMilliseconds
+  })
+  var tmin = d3.min(data, function(d, i) {
+    return d.timeMilliseconds
+  })
+
+	var xScale = d3.scaleLinear()
+			.domain([0, tmax]) // lap times
+			.range([0, 100]); // percent widths 
+
+  bars.enter()
+      .append("span")
+      .style("display", "block")
+			.style("margin-bottom", "4px")
+      .style("color", "white")
+      .style("background-color", "black")
+      .style("height", "1em")
+      .style("line-height", "normal")
+      .style("width", function(d) {
+        return xScale(d.timeMilliseconds)+"%";
+      })
+			.text(function(d){
+				return d.year + ": " + d.driver + " " + d.timeDisplay
+			});
+
+  bars.exit()
+      .remove();
+
+
+});
